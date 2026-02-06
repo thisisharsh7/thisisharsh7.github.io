a16490d0334da07e277ad5408f3ddb7b2115ff73
thisisharsh7 <9u.harsh@gmail.com>
2026-02-06 20:06:51 +0530

    Updates

--- _next/static/chunks/415a58d2feb23bf1.js
+++ _next/static/chunks/415a58d2feb23bf1.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/0db1eaf91770888c.js",
+  "static/chunks/6571140a1442c67a.js",
   "static/chunks/94a19a2443edf931.js",
   "static/chunks/e0430d2bcb505e69.js",
   "static/chunks/6a0ca61ced6d9d05.js",
   "static/chunks/212ad993efb819bc.css",
-  "static/chunks/turbopack-f931eca7c4ed4c87.js"
+  "static/chunks/turbopack-c8f1ab8644bd6ecf.js"
 ])
