ce90d32385180fe1cadbdcb6bb6650ed94708553
thisisharsh7 <thisisharsh7@users.noreply.github.com>
2026-04-26 16:33:38 +0000

    deploy: 648c6cea4c7938d4c6cb0fa5d56e432907cccce9

--- _next/static/IXTlpbO95loBx6zEsLeoa/_buildManifest.js
+++ _next/static/IXTlpbO95loBx6zEsLeoa/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/52745dc38fcc2cb0.js"
+    "static/chunks/cb98106b939f95e3.js"
   ],
   "/404": [
     "static/chunks/a30a745b921f0350.js"
--- _next/static/chunks/cb98106b939f95e3.js
+++ _next/static/chunks/cb98106b939f95e3.js
@@ -1,10 +1,10 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/46807266179711e8.js",
-  "static/chunks/a9a9efe84c738d99.js",
+  "static/chunks/d9db4cb3d5069664.js",
   "static/chunks/94a19a2443edf931.js",
   "static/chunks/de1a44aee700509a.js",
   "static/chunks/84d013c50c0a9dcb.js",
   "static/chunks/ff7818fbb76d71e8.js",
   "static/chunks/8dde21601d4021c1.js",
-  "static/chunks/turbopack-a4a55c415a808c02.js"
+  "static/chunks/turbopack-45341e0dc0471998.js"
 ])
